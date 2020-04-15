ca43ce68ce54cb2c6b519b4dfff6b9c9ff08df2c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 23:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(names, event) {
   messages = [];
   for(let i = 0; i < names.length; i++) {
-    messages.push(`Thank you, ${}, for the wonderful ${event} gift!`)
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
   return messages;
 }
