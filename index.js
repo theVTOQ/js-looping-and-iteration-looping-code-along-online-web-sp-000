96149dad26e2452e93085d074d40cc313fd6c5d9
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 22:58:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  messages = [];
+  for() {
+    messages.push(`Thank you, ${} for the wonderful ${event} gift!`)
+  }
+  return messages;
+}
