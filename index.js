033491a9881bc135550ae21c3d705e468fe0a17e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 22:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
 function writeCards(names, event) {
   messages = [];
-  for() {
-    messages.push(`Thank you, ${} for the wonderful ${event} gift!`)
+  for(let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${}, for the wonderful ${event} gift!`)
   }
   return messages;
 }
