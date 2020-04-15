8bc92a99750cd4d75b843723447e0d622ce0c639
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 23:05:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  messages = [];
-  for() {
-    messages.push(`Thank you, ${} for the wonderful ${event} gift!`)
+  let messages = [];
+  for(let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
   return messages;
+}
+
+function countDown(start) {
+  while (start >= 0) {
+    console.log(start);
+    start--;
+  }
 }
