42fa00d9ff78e9adab476fb0f75c4b776a757414
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 23:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  messages = [];
+  let messages = [];
   for(let i = 0; i < names.length; i++) {
     messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
